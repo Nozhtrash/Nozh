f2390380dff91c49e687c1e2be89cdbb6928223d
Nozhtrash <vigursgye@gmail.com>
2026-01-12 21:42:25 -0300

    Fix build errors: Resolve WORLD_LOADING, harden Mixins, suppress warnings

--- src/main/java/dev/nozh/mixin/MixinArmorStandEntity.java
+++ src/main/java/dev/nozh/mixin/MixinArmorStandEntity.java
@@ -7,11 +7,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ArmorStandEntity.class)
+@Mixin(net.minecraft.entity.Entity.class)
 public class MixinArmorStandEntity {
 
     @Inject(method = "isInvisible", at = @At("HEAD"), cancellable = true)
     private void nozh$forceInvisible(CallbackInfoReturnable<Boolean> info) {
+        if (!((Object) this instanceof ArmorStandEntity)) {
+            return;
+        }
         if (!RenderVisibilityDecider.isArmorStandVisible()) {
             info.setReturnValue(true);
         }
