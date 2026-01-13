181ab457e3daf9bbbd658960cc8f3fff50f48dbb
Nozhtrash <vigursgye@gmail.com>
2026-01-12 22:37:14 -0300

    Fix build errors: Resolve WORLD_LOADING and harden Mixins

--- src/main/java/dev/nozh/mixin/MixinItemFrameEntity.java
+++ src/main/java/dev/nozh/mixin/MixinItemFrameEntity.java
@@ -7,11 +7,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ItemFrameEntity.class)
+@Mixin(net.minecraft.entity.Entity.class)
 public class MixinItemFrameEntity {
 
     @Inject(method = "isInvisible", at = @At("HEAD"), cancellable = true)
     private void nozh$forceInvisible(CallbackInfoReturnable<Boolean> info) {
+        if (!((Object) this instanceof ItemFrameEntity)) {
+            return;
+        }
         if (!RenderVisibilityDecider.isItemFrameVisible()) {
             info.setReturnValue(true);
         }
